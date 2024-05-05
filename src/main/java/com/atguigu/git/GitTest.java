ae6dafc4a1a3a0a4ddd20098a3a71988bc5cb9ab
limitsaid <133259175+limitsaid@users.noreply.github.com>
2024-05-05 23:01:03 +0800

    master test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("hit-fix test ");
         System.out.println("ccc");
+        System.out.println("master test ");
     }
 }
